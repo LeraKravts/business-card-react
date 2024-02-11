b8821979a0dc1376fb7c6aa34306ec52ec9a4df2
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2024-02-11 16:49:14 +0100

    added bacis styles

--- src/About.js
+++ src/About.js
@@ -1,3 +1,16 @@
+
+
 export default function About(){
-    return (<div>About section is there</div>)
+    return (
+    <div className="about-section">
+        <div className="img-container">
+            {/* <img src={valeriaImg} alt="author-photo"/>  */}
+        </div>
+        <p>Valeria Kravtsova</p>
+        <p>Frontent Developer, Designer</p>
+        <p>portfolio.website </p>
+       
+        <div className="info-container"></div>
+        <div class="link-container"></div>
+    </div>)
 }
--- src/Interests.js
+++ src/Interests.js
@@ -1,3 +1,6 @@
 export default function Interests(){
-    return (<div>Interests section is there</div>)
+    return (<div>
+        <h2>Interests</h2>
+        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>)
 }
