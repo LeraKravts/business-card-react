759e61b85f9cbd321bfefb591426c4668d84dcd9
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2024-02-11 12:20:52 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import About from './About';
+import Info from './Info';
+import Interests from './Interests';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+   <div className='container'>
+    <About/>
+    <Info/>
+    <Interests/>
+    <Footer/>
+    
+   </div>
+  );
+}
+
+export default App;
